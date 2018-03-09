a6a672107c1fa8612e611b7482bc46646deb8c44
CrinaTara <vinaga.crina@gmail.com>
2018-03-09 16:40:48 +0200

    Fixed a bug in the edit-profile page and added some functionalities

--- src/app/user/edit-profile/edit-profile.component.ts
+++ src/app/user/edit-profile/edit-profile.component.ts
@@ -12,13 +12,12 @@ import * as firebase from 'firebase';
 export class EditProfileComponent implements OnInit {
 
   userForm: FormGroup;
-  changePasswordForm: FormGroup;
-  changeEmailForm: FormGroup;
+  // changePasswordForm: FormGroup;
+  changePassForm: FormGroup;
 
   messageDisplayed: string = '';
   messageDisplay: boolean = false;
   private showPictureProfile: boolean = true;
-  private showChangeEmail: boolean = false;
   private showChangePass: boolean = false;
   private showProfileInfo: boolean = false;
   
@@ -30,12 +29,12 @@ export class EditProfileComponent implements OnInit {
       homeBase: ['', Validators.required],
     })
 
-    this.changePasswordForm = this.fb.group({
-      newPassword: ['', [Validators.required, , Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-    })
+    // this.changePasswordForm = this.fb.group({
+    //   newPassword: ['', [Validators.required, , Validators.minLength(6)]],
+    //   confirmPassword: ['', Validators.required],
+    // })
 
-    this.changeEmailForm = this.fb.group({
+    this.changePassForm = this.fb.group({
       newEmail: ['', [Validators.required, Validators.pattern('^[_a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$')]]
     })
 
@@ -46,43 +45,71 @@ export class EditProfileComponent implements OnInit {
   }
 
   saveUserData(dataUser){
-    
+    console.log(dataUser);
+    this.db.collection("users").add({
+      firstName: dataUser.firstName,
+      lastName: dataUser.lastName,
+      bio: dataUser.bio, 
+      homeBase: dataUser.homeBase,
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+      this.userForm.patchValue({
+        firstName: '',
+        lastName: '',
+        bio: '',
+        homeBase: ''
+      });
+  });
   }
 
-  changeEmail(email){
-
-  }
-  updatePassword(userDetails) {
-   
-    var newPassword = this.changePasswordForm.value.newPassword;
-    var confirmPassword = this.changePasswordForm.value.confirmPassword;
-    if ((newPassword == '' || confirmPassword == '')) {
-      this.messageDisplay = true;
-      this.messageDisplayed = "All fields are required!";
-      setTimeout(() => {
-        this.messageDisplay = false;
-        this.messageDisplayed = '';
-      }, 2500);
-    }
-    else {
-      if (newPassword == confirmPassword) {
-        // cod
-
-      } else {
-        // this.ChangePassword.hide();
+  updatePassword(data){
+       var auth = firebase.auth();
+       let email =  data.newEmail;
+      return auth.sendPasswordResetEmail(email)
+       .then(() =>{
         this.messageDisplay = true;
-        this.messageDisplayed = "Password mismatch!";
-        this.changePasswordForm.patchValue({
-          newPassword: '',
-          confirmPassword: '',
-        });
-        setTimeout(() => {
-          this.messageDisplay = false;
-          this.messageDisplayed = '';
-        }, 2500);
-      }
-    }
+        this.messageDisplayed = 'Mail successfully sent! Please check your email.';
+      })
+       .catch((error) =>{ 
+           console.log(error);
+        })
   }
+  
+  // updatePassword(userDetails) {
+   
+  //   var newPassword = this.changePasswordForm.value.newPassword;
+  //   var confirmPassword = this.changePasswordForm.value.confirmPassword;
+  //   if ((newPassword == '' || confirmPassword == '')) {
+  //     this.messageDisplay = true;
+  //     this.messageDisplayed = "All fields are required!";
+  //     setTimeout(() => {
+  //       this.messageDisplay = false;
+  //       this.messageDisplayed = '';
+  //     }, 2500);
+  //   }
+  //   else {
+  //     if (newPassword == confirmPassword) {
+  //       // cod
+
+  //     } else {
+  //       // this.ChangePassword.hide();
+  //       this.messageDisplay = true;
+  //       this.messageDisplayed = "Password mismatch!";
+  //       this.changePasswordForm.patchValue({
+  //         newPassword: '',
+  //         confirmPassword: '',
+  //       });
+  //       setTimeout(() => {
+  //         this.messageDisplay = false;
+  //         this.messageDisplayed = '';
+  //       }, 2500);
+  //     }
+  //   }
+  // }
   
  toInfoProfile(){
     this.showPictureProfile = false;
@@ -100,12 +127,5 @@ export class EditProfileComponent implements OnInit {
     this.showProfileInfo = true;
     this.showChangePass = false;
   }
-  toChangeEmail(){
-    this.showChangePass = false;
-    this.showChangeEmail = true;
-  }
-  backChangePass(){
-    this.showChangeEmail = false;
-    this.showChangePass = true;
-  }
+
 }
